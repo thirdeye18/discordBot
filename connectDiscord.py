db7dc3fa288388779acfe482e9b27eded192fbe7
thirdeye18 <justinhammel62@gmail.com>
2021-07-11 10:43:06 -0700

    adding more chat commands/replies

--- connectDiscord.py
+++ connectDiscord.py
@@ -9,28 +9,28 @@ Description: Script for connecting and handling the interaction of the bot with 
                 as all the other files for the script.
 """
 
-## External library imports
-## os enables miscellaneous operating system interfaces(don't think this is needed here?)
-import os
+"""External library imports"""
+
 ## importing discord api, enables interaction with discord
 import discord
-from discord.ext import commands    # for running commands easier than using @client.event
 ## import for handling .env files, .env files store credentials
 from dotenv import load_dotenv
+## Needed to access the .env files
+import os
 ## allows random choices for the magic 8 ball feature
 import random
 
-load_dotenv()
+load_dotenv()   # loading dotenv to handle .env files
 ## Discord token read into program from an environment variable for security
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-description = '''An example bot to showcase the discord.ext.commands extension module. There are a number of utility commands being showcased here.'''
-
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
-
-## client represents connection to discord, similar to a sftp object
-## handles events, tracks state, and interacts with Discord APIs
+"""
+Classes bundle data and functionality together. This allows creation of a new
+object that can then have it's own methods.
+MyClient is the new object that interacts with Discord through the API by
+new methods with <async def>.
+"""
 class MyClient(discord.Client):
     async def on_ready(self):
         for guild in client.guilds:
@@ -46,11 +46,29 @@ class MyClient(discord.Client):
         ## First if loop checks to see if the message came from the bot prevents recursive calls
         if message.author == self.user:
             return
+
+        ## words for the bot to respond to as being not allowed in the server
+        bad_words = ['cheat', 'cheats', 'hack', 'hacks', 'internal', 'external', 'ddos', 'denial of service']
+
+        messageContent = message.content    # store the message in a string
+        ## loop through the message looking for the key words
+        if len(messageContent) > 0:     # make sure the message contains something
+            for word in bad_words:
+                if word in messageContent:
+                    await message.delete()  # delete the message if it has key words
+                    await message.channel.send('Do not say that!')
+
+        ## There can als be keyword commands for the bot to respond to, begin with either !, or ? to prevent confusion
         if message.content.startswith('!hello'):
-            await message.channel.send('Hello World!', mention_author=True)
-        if message.content.startswith('?magic8ball'):
+            await message.channel.send('Ello!', mention_author=True)
+        if message.content.endswith("hello?"):
+            await message.reply("No, I said ello, but that's close enough.")
+        if message.content.endswith('?magic8ball'):     #need to make sure the question ends with key word to trigger
             magicResponse = random.choice(['It is certain','As i see it, yes', 'Dont count on it', 'Without a doubt', 'Definitely', 'Very doubtful', 'Outlook not so good', 'My sources say no', 'My reply is no', 'Most likely', 'You may rely on it', 'Ask again later'])
             await message.channel.send(magicResponse)
+        ## command to disconnect bot on !quit
+        # if message.content.startswith('quit'):
+
 
     ## Greeting when new member joins the chat
 #    async def on_member_join(self, member):
@@ -68,6 +86,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.guilds = True
 
-bot.run(TOKEN)
+#bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 client = MyClient(intents = intents)
+#bot.run(TOKEN)
 client.run(TOKEN)
--- discordBot.py
+++ discordBot.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
-"""Import function to connect the bot to discord server"""
-import connectDiscord 
+"""
+Main for the Discord Bot
+Author: Justin Hammel
+Description: Main calls the Script for connecting and handling the interaction
+                of the bot with the server. I do not think this portion is
+                needed. I primarily added it to showcase calling another Script
+                from within the main.
+"""
+
+## Import function to connect the bot to discord server
+import connectDiscord
 
 def main():
     """Called at runtime"""
-    # Connecting the discord bot to the server
-    connectDiscord()
+    connectDiscord()    # This script is what does everythong
 
 if __name__ == "__main__":
     main()
