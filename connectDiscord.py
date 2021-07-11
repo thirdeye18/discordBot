d194022a2153aa042a957d87151ed2d65226e0eb
thirdeye18 <justinhammel62@gmail.com>
2021-07-11 14:56:59 -0700

    fixed sending dm's to members on join cleaned up some unused items and made comments more descriptive

--- connectDiscord.py
+++ connectDiscord.py
@@ -36,31 +36,38 @@ class MyClient(discord.Client):
         for guild in client.guilds:
             if guild.name == GUILD:
                 break
-        print(f'{client.user} has connected to the following server:')  # print the bots name
+        ## Printing the bots name to the console
+        print(f'{client.user} has connected to the following server:')
         print(f'{guild.name}(id: {guild.id})')  # print the server name and id
-        ## Print out the members of the server
-        members = '\n - '.join([member.name for member in guild.members])   # Puts all the guild members names in a list
+        ## Print out the members of the server in the console
+        ## First part puts all the member's names in a list
+        members = '\n - '.join([member.name for member in guild.members])
         print(f'Guild Members:\n - {members}')
 
     async def on_message(self, message):
-        ## First if loop checks to see if the message came from the bot prevents recursive calls
+        ## checks if the message came from the bot, prevents recursive calls
         if message.author == self.user:
             return
 
         ## words for the bot to respond to as being not allowed in the server
-        bad_words = ['cheat', 'cheats', 'hack', 'hacks', 'internal', 'external', 'ddos', 'denial of service']
+        bad_words = [
+            'cheat', 'cheats', 'hack', 'hacks', 'internal', 'external', 'ddos',
+            'denial of service'
+        ]
 
         messageContent = message.content    # store the message in a string
         ## loop through the message looking for the key words
-        if len(messageContent) > 0:     # make sure the message contains something
+        if len(messageContent) > 0: # make sure the message contains something
             for word in bad_words:
                 if word in messageContent:
-                    await message.delete()  # delete the message if it has key words
+                    await message.delete()  # delete the message if bad_words
                     await message.channel.send('Do not say that!')
 
-        ## There can als be keyword commands for the bot to respond to, begin with either !, or ? to prevent confusion
+        ## keyword detection uses existing string methods to parse messages
+        ## keyword commands for the bot to respond to, start with !
         if message.content.startswith('!hello'):
             await message.channel.send('Ello!', mention_author=True)
+        ## can also check endswith to detect questions
         if message.content.endswith("hello?"):
             await message.reply("No, I said ello, but that's close enough.")
         if message.content.endswith('?magic8ball'):     #need to make sure the question ends with key word to trigger
@@ -71,22 +78,17 @@ class MyClient(discord.Client):
 
 
     ## Greeting when new member joins the chat
-#    async def on_member_join(self, member):
-#        guild = member.guild
-#        if guild.system_channel is not None:
-#            await guild.system_channel.send(f'Ello {0.mention}, fancy a cuppa?'.format(member, guild))
-#            await member.create_dm()    # Next part will send a DM to the member that just joined the channel
-#            await member.dm_channel.send(f'Ello {member.name}, lovely server innit!')
+    async def on_member_join(self, member):
+        await member.create_dm()     # Next part will send a DM to the member that just joined the channe
+        await member.dm_channel.send(
+            f'Hi {member.name}, welcome to the mad house. Fancy a cuppa?'
+        )
 
-#        if message.content == 'hello':
-#            response = "No, I said ello, but that's close enough"
-#            await message.channel.send(response)
-
+## Intents are like permissions for what the bot can do
 intents = discord.Intents.default()
-intents.members = True
-intents.guilds = True
+intents.members = True  # this intent is required to send messages
+intents.guilds = True   # this would be allowed if I wanted the bot to be able to kick members
 
-#bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+## This is the portiong that actually initializes and starts the bot
 client = MyClient(intents = intents)
-#bot.run(TOKEN)
 client.run(TOKEN)
